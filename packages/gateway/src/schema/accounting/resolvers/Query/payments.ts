321db9aa6b06256e20df55c3b930f466832ecbf6
Amadeus Beckmann <amadeus@beckmann.io>
2025-03-19 22:03:51 +0100

    Implement payments query resolver

--- packages/gateway/src/schema/accounting/resolvers/Query/payments.ts
+++ packages/gateway/src/schema/accounting/resolvers/Query/payments.ts
@@ -1,6 +1,30 @@
+import { GraphQLError } from "graphql/error";
+import { z } from "zod";
+import { paymentSchema } from "~/adapter/in/dto/payment.ts";
+import { invokeService } from "~/infrastructure/service";
 import type { QueryResolvers } from "~/schema/types.generated";
 
 
 export const payments: NonNullable<QueryResolvers["payments"]> = async (_parent, _arg, _ctx) => {
-  return [];
+  if (_ctx.session?.role !== "ADMIN") {
+    throw new GraphQLError("Not authorized");
+  }
+
+  const payments = await invokeService("accounting", {
+    endpoint: `GET /payments`,
+    headers: {
+      "X-Request-ID": _ctx.requestId,
+    },
+    output: z.array(paymentSchema),
+  });
+
+  return payments.map(payment => {
+    return {
+      id: payment.id,
+      amount: payment.amount,
+      status: payment.status,
+      createdAt: payment.createdAt,
+      customer: null,
+    };
+  });
 };
