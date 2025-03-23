d1babe1f629e698dd57974f5c6bbdf188663a8a0
Amadeus Beckmann <amadeus@beckmann.io>
2025-03-23 22:32:31 +0100

    Add function to invoke another micro-service

--- packages/shared/micro/service.go
+++ packages/shared/micro/service.go
@@ -2,13 +2,19 @@ package micro
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
+	"slices"
+	"strings"
 	"time"
 )
+
+var methods = []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodDelete}
 
 func Run(mux *http.ServeMux, serverAddr string) (err error) {
 	server := &http.Server{
@@ -39,4 +45,47 @@ func Run(mux *http.ServeMux, serverAddr string) (err error) {
 	defer cancel()
 
 	return server.Shutdown(ctx)
+}
+
+func Invoke[TInput any, TOutput any](ctx context.Context, endpoint string, requestID *string, input TInput) (TOutput, error) {
+	var httpClient http.Client
+	var output TOutput
+
+	segments := strings.Split(endpoint, " ")
+	if len(segments) != 2 {
+		return output, fmt.Errorf("invalid endpoint: %s", endpoint)
+	}
+
+	method := segments[0]
+	url := segments[1]
+
+	if slices.Contains(methods, method) == false {
+		return output, fmt.Errorf("invalid method: %s", method)
+	}
+
+	req, err := http.NewRequest(method, fmt.Sprintf("http://%s", url), nil)
+	if err != nil {
+		return output, err
+	}
+
+	// Request with context
+	req = req.WithContext(ctx)
+
+	if requestID != nil {
+		req.Header.Set("X-Request-ID", *requestID)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return output, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return output, fmt.Errorf("failed to invoke endpoint: %s (status %d)", resp.Status, resp.StatusCode)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&output)
+
+	return output, err
 }
