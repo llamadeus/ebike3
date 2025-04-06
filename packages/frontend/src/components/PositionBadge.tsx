cd266b986bde58ebbe0ce266ce31e8f281c26ebe
Amadeus Beckmann <amadeus@beckmann.io>
2025-04-06 14:23:50 +0200

    Allow overriding badge classes

--- packages/frontend/src/components/PositionBadge.tsx
+++ packages/frontend/src/components/PositionBadge.tsx
@@ -1,4 +1,5 @@
 import { Vec2d } from "~/gql/graphql";
+import { cn } from "~/lib/utils";
 
 
 interface Props {
@@ -6,11 +7,16 @@ interface Props {
    * The vehicle to display the position for.
    */
   position: Vec2d;
+
+  /**
+   * The class name to apply to the badge.
+   */
+  className?: string;
 }
 
 export function PositionBadge(props: Props) {
   return (
-    <div className="flex items-center">
+    <div className={cn("flex items-center", props.className)}>
       <div className="rounded-full bg-foreground/10 px-2">
         {Math.round(props.position.x * 100) / 100}
         {"; "}
